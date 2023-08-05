228a9d894bb0d9f5a0ef87722ce242c1ef15153d
Isa Gadaev <depressingsupremacy@outlook.com>
2023-08-05 01:57:07 +0000

    Add virtual truck control plugin

--- extensions/src/index.ts
+++ extensions/src/index.ts
@@ -2,10 +2,13 @@ import { ExtensionContext } from "@foxglove/studio";
 import { initCartPolePanel } from "./CartPolePanel";
 import { initIFramePanel } from "./IFramePanel";
 import { initButtonPanel } from "./ButtonPanel";
+import { initTruckControlPanel } from "./TruckControlPanel";
+
 import "./style.css";
 
 export function activate(extensionContext: ExtensionContext): void {
   extensionContext.registerPanel({ name: "CartPole Panel", initPanel: initCartPolePanel });
   extensionContext.registerPanel({ name: "IFrame Panel", initPanel: initIFramePanel });
   extensionContext.registerPanel({ name: "Button Panel", initPanel: initButtonPanel });
+  extensionContext.registerPanel({ name: "truck control", initPanel: initTruckControlPanel });
 }
